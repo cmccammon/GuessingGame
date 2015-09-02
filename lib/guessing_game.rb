07a4339e8e0e0b61fd9a29843492001878b41ee2
Chris McCammon <cmccammon@gmail.com>
2015-09-01 21:14:22 -0500

    final commit all comments included

--- lib/guessing_game.rb
+++ lib/guessing_game.rb
@@ -1,47 +1,47 @@
 
 
-
+#This game takes a random number between 0-100 and asks the user to try and guess it.
+#The user has 5 guesses to get it right. The program will tell the user if thier guess
+#is too high or too low.
 
 
 class Game
 
+  def initialize
+    @turns = []               # => []
+    @rand_number = rand(100)  # Generate the reandom number
+  end
 
-
-def initialize
-  @turns = []               # => []
-  @rand_number = rand(100)  # => 21
-end
-
-
-def input
-  puts "Guess a number between 0-100 please.  >"                # => nil
-  @guess = gets.chomp.to_i                                      # ~> NoMethodError: undefined method `chomp' for nil:NilClass
-  while @turns.include?(@guess)
-    puts "Didn't we try that one already? Hmmmm Pick again.  >"
+  #Take the input, check it against the guess array and prevent duplicate inputs
+  #It also passes the input on to an array to track the number of guesses
+  def input
+    puts "Guess a number between 0-100 please.  >"
     @guess = gets.chomp.to_i
-  end
-  @turns << @guess
-end
-
-def check
-  if @rand_number > @guess
-      puts "your guess is too low"
-    elsif @rand_number < @guess
-      puts "your guess is too high"
-    elsif @rand_number == @guess
-      puts "Congratulations you guessed it.  That was really fucking hard!"
+    while @turns.include?(@guess)
+      puts "Didn't we try that one already? Hmmmm Pick again.  >"
+      @guess = gets.chomp.to_i
     end
-end
-
-def begin
-  while @turns.length < 5 do                                     # => true
-    input
-    check
+    @turns << @guess
   end
-  puts "You wern't even close. Nice try it was #{@rand_number}"
-end
-
-
+  #Checks the guess to see if it is too high or too low compaired to the random number
+  #also checks if it is the correct number.
+  def check
+    if @rand_number > @guess
+        puts "your guess is too low"
+      elsif @rand_number < @guess
+        puts "your guess is too high"
+      elsif @rand_number == @guess
+        puts "Congratulations you guessed it.  That was really fucking hard!"
+    end
+  end
+  #this actually runs the program through.
+  def begin
+    while @turns.length < 5 do
+      input
+      check
+    end
+    puts "You wern't even close. Nice try it was #{@rand_number}"
+  end
 end
 
 
@@ -49,34 +49,3 @@ end
 
 game = Game.new  # => #<Game:0x007fd04c0b8438 @turns=[], @rand_number=21>
 game.begin
-# def rand_number
-#   rand(100)      # => 8, 28
-# end
-
-
-
-# puts "number is #{rand_number}"  # => nil
-
-# rand_number  # => 28
-
-
-
-# def guess
-#   if @rand_number = input
-#     puts "You got it right"
-#     elsif @rand_number > input
-#       puts "your guess is too low"
-#     elsif @rand_number < input
-#       puts "your guess is too high"
-#   end
-#end
-
-
-# >> Guess a number between 0-100 please.  >
-
-# ~> NoMethodError
-# ~> undefined method `chomp' for nil:NilClass
-# ~>
-# ~> /Users/chris/theironyard/week2/day2/numberguess/lib/guessing_game.rb:18:in `input'
-# ~> /Users/chris/theironyard/week2/day2/numberguess/lib/guessing_game.rb:38:in `begin'
-# ~> /Users/chris/theironyard/week2/day2/numberguess/lib/guessing_game.rb:51:in `<main>'
